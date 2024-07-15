119e1f64d05e3517f2581a1e52ec0e42842f60ba
Pham Ngoc Bao <1005hanhphuc@gmail.com>
2024-07-15 22:53:08 +0700

    Modify Search API

--- FEventopia.Repositories/Repositories/EventRepository.cs
+++ FEventopia.Repositories/Repositories/EventRepository.cs
@@ -9,6 +9,7 @@ using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using FuzzySharp;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -29,32 +30,25 @@ namespace FEventopia.Repositories.Repositories
                 return text;
             }
 
-            text = Regex.Replace(text, "[^a-zA-Z0-9 ]", " ");
-
-            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var normalizedString = text.Normalize(NormalizationForm.FormC).ToLower();
             var stringBuilder = new StringBuilder();
 
-            foreach (var c in normalizedString)
+            foreach (char c in normalizedString)
             {
-                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
-                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
-                {
-                    stringBuilder.Append(c);
-                }
+                stringBuilder.Append(VietNameseAccentUtils.GetUnaccentedCharacter(c));
             }
 
             return stringBuilder
                 .ToString()
-                .Normalize(NormalizationForm.FormC)
+                .Normalize(NormalizationForm.FormKD)
                 .ToLowerInvariant();
         }
+
         private static IEnumerable<Event> SearchData(IEnumerable<Event> data, string keyword)
         {
-            string normalizedKeyword = UnsignName(keyword);
-
-            return data.Where(item => UnsignName(item.EventName).Contains(normalizedKeyword));
+            return data.Where(e => Fuzz.PartialRatio(UnsignName(keyword), UnsignName(e.EventName)) >= 60)
+                .OrderByDescending(e => Fuzz.PartialRatio(UnsignName(keyword), UnsignName(e.EventName)));
         }
-
 
         public async Task<Event?> GetEventWithDetailByIdAsync(string id)
         {
@@ -79,6 +73,44 @@ namespace FEventopia.Repositories.Repositories
         {
             var eventLists = await _eventDAO.GetAllAsync();
             return eventLists.Where(e => e.Status.ToUpper().Equals("EXECUTE") || e.Status.ToUpper().Equals("POST")).ToList();
+        }
+    }
+
+    public class VietNameseAccentUtils
+    {
+        private static readonly Dictionary<char, char> AccentMap = new Dictionary<char, char>()
+        {
+            {'à', 'a'}, {'á', 'a'}, {'ả', 'a'}, {'ã', 'a'}, {'ạ', 'a'},
+            {'è', 'e'}, {'é', 'e'}, {'ẻ', 'e'}, {'ẽ', 'e'}, {'ẹ', 'e'},
+            {'ì', 'i'}, {'í', 'i'}, {'ỉ', 'i'}, {'ĩ', 'i'}, {'ị', 'i'},
+            {'ò', 'o'}, {'ó', 'o'}, {'ỏ', 'o'}, {'õ', 'o'}, {'ọ', 'o'},
+            {'ù', 'u'}, {'ú', 'u'}, {'ủ', 'u'}, {'ũ', 'u'}, {'ụ', 'u'},
+            {'đ', 'd'},
+
+            {'â', 'a'}, {'ă', 'a'},
+            {'ê', 'e'}, {'ô', 'o'},
+            {'ư', 'u'}, {'ơ', 'o'},
+            {'ỳ', 'y'}, {'ý', 'y'}, {'ỷ', 'y'}, {'ỹ', 'y'}, {'ỵ', 'y'},
+
+            {'ấ', 'a'}, {'ầ', 'a'}, {'ẩ', 'a'}, {'ẫ', 'a'}, {'ậ', 'a'},
+            {'ắ', 'a'}, {'ằ', 'a'}, {'ẳ', 'a'}, {'ẵ', 'a'}, {'ặ', 'a'},
+            {'ế', 'e'}, {'ề', 'e'}, {'ể', 'e'}, {'ễ', 'e'}, {'ệ', 'e'},
+            {'ố', 'o'}, {'ồ', 'o'}, {'ổ', 'o'}, {'ỗ', 'o'}, {'ộ', 'o'},
+            {'ớ', 'o'}, {'ờ', 'o'}, {'ở', 'o'}, {'ỡ', 'o'}, {'ợ', 'o'},
+            {'ứ', 'u'}, {'ừ', 'u'}, {'ử', 'u'}, {'ữ', 'u'}, {'ự', 'u'},
+        };
+
+        public static char GetUnaccentedCharacter(char @char)
+        {
+            char lowerChar = Char.ToLowerInvariant(@char);
+            if (AccentMap.ContainsKey(lowerChar))
+            {
+                return AccentMap[lowerChar];
+            }
+            else
+            {
+                return @char;
+            }
         }
     }
 }
